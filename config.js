43cd47688046d50fd52f203367ba06fecb535db0
sebastianluna-dev <sebastian.luna.developer@gmail.com>
2022-11-12 03:44:22 -0600

    env variable fixed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_USER || "localhost"
+export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_USER = process.env.DB_USER || "root"
 export const DB_PASSWORD = process.env.DB_PASSWORD || "naitsabes123"
 export const DB_NAME = process.env.DB_NAME || "coderhouse_db"
